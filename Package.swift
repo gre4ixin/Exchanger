c77b7a089a8735cb236797c320a4d8cfdca39ece
Pavel Grechikhin <pav.gre4ixin@yandex.ru>
2022-08-25 02:38:18 +0400

    Edit package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Exchanger", "ExchangerTests"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", exact: .init(15, 0, 0)),
+        .package(url: "https://github.com/Moya/Moya.git", exact: Version(15, 0, 0)),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
